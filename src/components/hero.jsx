603b1b9c243ea9b7d9bbd88fa627fd72057c93c3
Mayurraj Singh Rathore <mayurraj.singh@webcluesinfotech.com>
2026-08-12 11:10:49 +0530

    added

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,8 +1,25 @@
+import hero1 from "../../public/images/hero1.png";
+import hero2 from "../../public/images/hero2.png";
+import hero3 from "../../public/images/hero3.png";
+
+
 export default function Hero(){
 
     return(
             <section className="bg-white px-4 py-12 text-black md:px-8 flex">
+       <div>  
 
+      <div className="h-120 w-120">
+        <img src="/images/hero1.png" alt="Home preview" className="h-auto w-full" />
+      </div>
+
+      <div className="bg-white">
+        <h2 >Private Channels <br />
+Exclusive Access</h2>
+
+      </div>
+
+    </div>
 
 
 
